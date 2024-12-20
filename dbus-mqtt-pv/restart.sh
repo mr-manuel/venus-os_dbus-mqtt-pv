331177a7f939fcaba59c165dc91171a72892a920
Manuel <mr-manuel@outlook.it>
2024-12-20 08:57:30 +0100

    Made install and upgrade easier

--- dbus-mqtt-pv/restart.sh
+++ dbus-mqtt-pv/restart.sh
@@ -2,4 +2,15 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SERVICE_NAME=$(basename $SCRIPT_DIR)
 
-kill $(pgrep -f "python $SCRIPT_DIR/$SERVICE_NAME.py")
+echo
+echo "Restarting $SERVICE_NAME..."
+
+pid=$(pgrep -f "python $SCRIPT_DIR/$SERVICE_NAME.py")
+if [ -n "$pid" ]; then
+    svc -t /service/$SERVICE_NAME
+    echo "done."
+else
+    echo "driver is not running!"
+fi
+
+echo
